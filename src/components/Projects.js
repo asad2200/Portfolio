ad1534bb7a322e2324b2856e163254a9a5de3c4d
asad2200 <asadjakhavala92@gmail.com>
2021-07-07 18:50:38 +0530

    change project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,31 +6,31 @@ const project5=""
 const project6=""
 export const projects = [
     {
-      name: "Job Portal (django)",
-      description: ['This is the jobortal website.', 
-      'where employer can post a job and job seeker aplly for the job.',
-      'Employer and jobseeker can also "CHAT".',
-      "Also employer can create ZOOM MEETING from website itself."
-      ],
-      image: project2,
-      source_code:"https://github.com/asad2200/JobPortal",
-      live:"https://jobportal-asad.herokuapp.com/",
-    },
-    {
       name: "Stock Tracker (django)",
-      description: ['This app tracks stocks in realtime.',
-      'For real time update " DJANGO CELERY, & DJANGO CHANNELS ".',
-      '"yahoo-fin" library used for getting data.'
+      description: ["It's Stock Tracking APP.",
+      'Stock data updates in REAL-TIME',
+      "For the real-time update, I used 'django-channels'.",
+      "Data comes from the 'yahoo-finance' website by 'django-celery.'"
       ],
       image: project1,
       source_code:"https://github.com/asad2200/Stock-Tracker",
     },
     {
+      name: "Job Portal (django)",
+      description: ["It's App where EMPLOYER POST JOBS and JOBSEEKER APPLY.", 
+      'Employer and jobseeker can chat within the app.',
+      'Also, the Employer can schedule a ZOOM meeting for interviews within the app.',
+      "ZOOM meeting feature adds through ZOOM API."
+      ],
+      image: project2,
+      source_code:"https://github.com/asad2200/JobPortal",
+    },
+    {
       name: "authsimple (django-package)",
       description: [
-        'authsimple is a Django app to implement "BASIC AUTHENTICATION" in django project.',
-        'You can use it by this command "pip install django-authsimple"',
-        'This package contain "EMAIL VALIDATION & FORGOT PASSWORD on EMAIL" as well'
+        "authsimple is a package that includes an 'authentication system in django project'.",
+        "It can be installed using 'pip install Django-authsimple'.",
+        "This package also contain EMAIL VALIDATION & FORGOT PASSWORD on EMAIL as well"
       ],
       image: project4,
       source_code:"https://github.com/asad2200/authsimple",
@@ -39,9 +39,9 @@ export const projects = [
     {
       name: "Github User Detail",
       description: [
-        'This is created by " REACTJS., FIREBASE"',
+        "This is created by REACTJS., FIREBASE",
         'Basically we can search github profile by username and see detail.',
-        'Github profile info fetch by " GITHUB API. "'
+        "Github profile info fetch by GITHUB API."
       ],
       image: project6,
       source_code:"https://github.com/asad2200/github-user-search",
@@ -49,16 +49,16 @@ export const projects = [
     },
     {
       name: "Url Shortener (django)",
-      description: ['This project make long url short.'],
+      description: ['This project make a url short.'],
       image: project3,
       source_code:"https://github.com/asad2200/UrlShortener",
       live:"https://url-short-asad.herokuapp.com/",
     },  
     {
       name: "Automart (frontend + backend)",
-      description: ['This is website for " CAR ACCESSORIES SHOP. "',
-      'Frontend created using " REACTJS "',
-      'Backend is created using " django-rest-framework "'
+      description: ["It's a Website for a 'car accessories shop'.",
+      "Frontend created using REACTJS. ",
+      "Backend is created using 'django-rest-framework'."
       ],
       image: project5,
       source_code:"https://github.com/asad2200/automart-frontend",
@@ -66,7 +66,7 @@ export const projects = [
     },   
     {
       name: "Bookkeeping (DesktopApp JAVA)",
-      description: ['This is " INVENTORY MANAGMENT SYSTEM" in " JAVA (SWING). "',
+      description: ["It's INVENTORY MANAGMENT SYSTEM in JAVA(SWING).",
       'This is first big project i created. :)',
       ],
       image: project5,
